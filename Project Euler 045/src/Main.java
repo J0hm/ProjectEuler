0ac095bef475e867a8a4edadeb0849940d2292f1
J0hm <johnkerr2003@gmail.com>
2020-02-09 17:04:58 -0500

    Problem 45

---
+++ Project Euler 045/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+	
+	public static boolean isPentagonal(long n) {
+		double i = (1+Math.sqrt(24*n+1))/6;
+		
+		return i - Math.floor(i) == 0;
+	}
+	
+	public static boolean isHexagonal(long n) {
+		// n = i(2i-1)
+		// 
+		double i = (1+Math.sqrt(1+(8*n)))/4;
+		return i - Math.floor(i) == 0; 
+	}
+	
+	public static long findTriplet(long index) {
+		long triangle = (index * (index+1))/2;
+		
+		while(!(isHexagonal(triangle) && isPentagonal(triangle))) {
+			index++;
+			triangle = (index * (index+1))/2;
+		}
+		
+		return triangle;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(findTriplet(300L));
+	}
+
+}
