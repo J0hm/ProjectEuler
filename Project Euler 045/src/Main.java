1ec23a896dae7076a5623fa17b880f506dedafae
J0hm <johnkerr2003@gmail.com>
2020-02-09 17:18:06 -0500

    Problem 45

--- Project Euler 045/src/Main.java
+++ Project Euler 045/src/Main.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class Main {
 	
 	public static boolean isPentagonal(long n) {
@@ -26,7 +28,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(findTriplet(300L));
+		System.out.println(findTriplet(300));
 	}
 
 }
