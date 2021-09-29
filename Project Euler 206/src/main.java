f8c1057374e891b043d5d955788f01243bf65163
J0hm <johnkerr2003@gmail.com>
2021-09-29 17:11:04 -0400

    Finished problem 206

--- Project Euler 206/src/main.java
+++ Project Euler 206/src/main.java
@@ -1,27 +1,29 @@
+import java.math.BigInteger;
 import java.util.regex.*;
 
 public class main {
-    public static Pattern pattern = Pattern.compile("1\\d2\\d3\\d4\\d5\\d6\\d7\\d8\\d9\\d0", Pattern.CASE_INSENSITIVE);
+    public static Pattern pattern = Pattern.compile("1\\d2\\d3\\d4\\d5\\d6\\d7\\d8\\d9", Pattern.CASE_INSENSITIVE);
 
 
     public static boolean isConcealedNumber(long n){
         String s = Long.toString(n);
+        //System.out.println(s);
         Matcher matcher = pattern.matcher(s);
         return matcher.matches();
     }
 
     public static void solve(){
-        long n;
-        for(long i = (long) Math.sqrt(102030405060708090L); i <= (long) Math.sqrt(192939495969798990L); i++){
-            n = (long) Math.pow(i, 2);
-            if(isConcealedNumber(n)){
-                System.out.println(n);
-                break;
-            }
+        long n = 138902663; //Math.sqrt(19293949596979899), our upper limit
+        
+        while(!isConcealedNumber(n*n)){
+            n -= 2;
         }
+
+        System.out.println(n*10); // result is n*10 because of analysis done to make domain smaller
     }
 
     public static void main(String[] args) throws Exception {
         solve();
+        
     }
 }
